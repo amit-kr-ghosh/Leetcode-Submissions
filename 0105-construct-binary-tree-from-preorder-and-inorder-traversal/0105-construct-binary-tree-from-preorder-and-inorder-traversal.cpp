423d94a2a7a1bafd4c0551ed37fe06f4e08fbb72
Amit Kumar Ghosh <135455448+amit-kr-ghosh@users.noreply.github.com>
2025-10-03 15:00:43 +0530

    Time: 4 ms (57.96%), Space: 27.3 MB (72.4%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -19,32 +19,28 @@ public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int n = preorder.size();
         if (n == 0) return nullptr;
-        // map value -> index in inorder array
+        
         for (int i = 0; i < (int)inorder.size(); ++i) {
             inIndex[inorder[i]] = i;
         }
-        // store reference to preorder for easier access
+      
         pre = &preorder;
         return build(0, n - 1, 0, n - 1);
     }
 
 private:
     unordered_map<int,int> inIndex;
-    vector<int>* pre; // pointer to preorder vector
+    vector<int>* pre;
 
-    // build tree from preorder[preL..preR] and inorder[inL..inR]
     TreeNode* build(int preL, int preR, int inL, int inR) {
         if (preL > preR || inL > inR) return nullptr;
         int rootVal = (*pre)[preL];
         TreeNode* root = new TreeNode(rootVal);
 
-        int mid = inIndex[rootVal];               // index of root in inorder
-        int leftSize = mid - inL;                 // number of nodes in left subtree
+        int mid = inIndex[rootVal];               
+        int leftSize = mid - inL;                 
 
-        // left subtree: preorder [preL+1 .. preL+leftSize], inorder [inL .. mid-1]
         root->left = build(preL + 1, preL + leftSize, inL, mid - 1);
-
-        // right subtree: preorder [preL+leftSize+1 .. preR], inorder [mid+1 .. inR]
         root->right = build(preL + leftSize + 1, preR, mid + 1, inR);
 
         return root;
