69c79d4ca32bd6480275057fe95c01dbf15f4cea
Amit Kumar Ghosh <135455448+amit-kr-ghosh@users.noreply.github.com>
2026-01-29 11:28:19 +0530

    Time: 0 ms (100%), Space: 7.8 MB (70.6%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 2) return n;  // base cases
-        int first = 1, second = 2; // ways to reach step 1 and step 2
-        for (int i = 3; i <= n; i++) {
-            int current = first + second; 
-            first = second;
-            second = current;
+        // if(n == 0||n==1) return 1;
+        // return climbStairs(n-1)+climbStairs(n-2);
+
+        if(n == 0||n==1) return 1;
+
+        int prev1 = 1;
+        int prev2 =1;
+        int cur = 0;
+        for(int i = 2;i<=n;i++){
+            cur  = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
+            
         }
-        return second; // total ways for n steps
+
+        return cur;
     }
 };
