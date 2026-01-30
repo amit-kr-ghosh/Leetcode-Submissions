0fb14db99725613f86004296b8a479f50a2a2552
Amit Kumar Ghosh <135455448+amit-kr-ghosh@users.noreply.github.com>
2026-01-30 16:00:33 +0530

    Time: 0 ms (100%), Space: 9.9 MB (61.57%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
+void subseq(int index, vector<int> & nums , vector<int> &curr , vector<vector<int>> &res , int n){
+            if(index == n){
+                res.push_back(curr);
+            
+            return;
+          }
+
+          //include
+          curr.push_back(nums[index]);
+          subseq(index+1,nums,curr,res,n);
+
+          //exclude
+          curr.pop_back();
+          subseq(index+1,nums,curr,res,n);
+          }
+
     vector<vector<int>> subsets(vector<int>& nums) {
           int n = nums.size();
-        int total = 1 << n;
-        vector<vector<int>> res;
-        for (int mask = 0; mask < total; mask++) {
-            vector<int> subset;
-            for (int i = 0; i < n; i++) {
-                if (mask & (1 << i)) subset.push_back(nums[i]);
-            }
-            res.push_back(subset);
-        }
-        return res;
+          vector<vector<int>> res;
+          vector<int> curr;
+
+          
+subseq(0,nums,curr,res,n);
+          return res;
         
     }
 };
