4e438341d691a6e7f808caa1f285a267b1216670
Amit Kumar Ghosh <135455448+amit-kr-ghosh@users.noreply.github.com>
2026-06-05 11:09:47 +0530

    Time: 50 ms (6.99%), Space: 27.5 MB (27.85%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size();
-        int m = text2.size();
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-
-        return dp[n][m];
+        vector<vector<int>> dp (text1.size(),vector<int> (text2.size(),-1) );
+        return lcs(text1,text2,0,0,dp);
+         
     }
+     int lcs(string &text1,string &text2 , int id1 , int id2 , vector<vector<int>>&dp){
+
+            if(id1==text1.size()||id2 == text2.size()){
+                return 0;
+            }
+            if(dp[id1][id2]!=-1){
+                return dp[id1][id2];
+            }
+    
+
+            if(text1[id1] == text2[id2]){
+               return dp[id1][id2] = 1+ lcs(text1,text2,id1+1,id2+1,dp);
+            }
+
+                return dp[id1][id2] = max(lcs(text1,text2,id1+1,id2,dp),lcs(text1,text2,id1,id2+1,dp));
+            
+          }
+
 };
