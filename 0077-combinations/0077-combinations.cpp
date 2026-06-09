ca95502da1b6fb9d3f25df9a7cbb3b589336d222
Amit Kumar Ghosh <135455448+amit-kr-ghosh@users.noreply.github.com>
2026-06-09 10:10:26 +0530

    Time: 63 ms (53.78%), Space: 110 MB (22.91%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -11,23 +11,26 @@ public:
     }
 
 
-        void comb(int idx , int k , vector<int> &temp, vector<vector<int>> &ans ,int n){
-            if(k==0){
-                ans.push_back(temp);
-                return;
-            }
-
-            for(int i = idx ;i<n+1;i++){
-                //include
-                temp.push_back(i);
-
-                //call next
-                comb(i+1,k-1,temp,ans,n);
-
-                //exclude
-                temp.pop_back();
-            }
+    void comb(int idx , int k , vector<int> &temp, vector<vector<int>> &ans ,int n){
+        if(k==0){
+            ans.push_back(temp);
+            return;
         }
-        
+        if(idx == n+1){
+            return;
+        }
+
+    
+        //include
+        temp.push_back(idx);
+        //call next
+        comb(idx+1,k-1,temp,ans,n);
+
+        //exclude
+        temp.pop_back();
+        //call next
+        comb(idx+1,k,temp,ans,n);
+    }
+    
     
 };
