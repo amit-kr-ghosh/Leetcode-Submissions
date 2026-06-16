f68f09cb9da030dd527bc17836fd9f6a8e25b58d
Amit Kumar Ghosh <135455448+amit-kr-ghosh@users.noreply.github.com>
2026-06-16 14:22:02 +0530

    Time: 8 ms (26.89%), Space: 39.8 MB (69.7%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string res;
+        for(char c:s){
+            if(islower(c)){
+                res.push_back(c);
+            }
+            else if(c == '*'){
+                if(!res.empty()){
+                    res.pop_back();
+                }
+            }
+            else if(c == '#'){
+                res.append(res);
+            }
+            else if(c== '%'){
+                reverse(res.begin(),res.end());
+            }
+        }
+        return res;
+
+        
+    }
+};
