284d106d619475d4b062ae394cd37458ecc732a6
Amit Kumar Ghosh <135455448+amit-kr-ghosh@users.noreply.github.com>
2026-06-24 14:11:50 +0530

    Time: 385 ms (30.58%), Space: 8.8 MB (37.61%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,49 +1,52 @@
 class Solution {
 public:
-    void solveSudoku(vector<vector<char>>& board) {
-        sud(board);
+    void solveSudoku(vector<vector<char>>& board) { 
+        solve(board);
+       
     }
 
-    bool sud(vector<vector<char>>&board){
-        for(int r = 0;r<9;r++){
-            for(int c = 0;c<9;c++){
-                if(board[r][c] == '.'){
-                    for(char x = '1';x<='9';x++){
-                        if(isSafe(r,c,x,board)){
-                            board[r][c] = x;
-                            if(sud(board)){
+    bool solve(vector<vector<char>>& board){
+        for(int  i = 0;i<9;i++){
+            for(int  j = 0;j<9;j++){
+                if(board[i][j] == '.'){
+                    for(char c = '1';c<='9';c++){
+                        if(canplace(c,i,j,board)){
+                            board[i][j] = c;
+                            if(solve(board)){
                                 return true;
                             }
-                            board[r][c] = '.';
+                            board[i][j] = '.';
                         }
                     }
-                return false;
+                    return false;
 
                 }
-              
+
             }
         }
         return true;
     }
 
-    bool isSafe(int r , int c , char x , vector<vector<char>> &board){
-        for(int i = 0;i<9;i++){
+    bool canplace(char ch , int r , int c , vector<vector<char>> & board){
+        for(int  i = 0;i<9;i++ ){
             //check row
-            if(board[r][i]== x){
+            if(board[r][i]== ch){
                 return false;
             }
 
             //check column
-            if(board[i][c] == x){
+            if(board[i][c] == ch){
                 return false;
             }
 
-            //check 3x3 grid
-            if(board[(r/3)*3+i/3][(c/3)*3+i%3]== x){
+            // check 3x3 grid
+            if(board[(r/3)*3 + i/3 ][(c/3)*3 + i%3 ]==ch){
                 return false;
             }
         }
         return true;
+
     }
+
 
 };
