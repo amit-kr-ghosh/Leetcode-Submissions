34375da29fe93ea912eb080d753446ddf951802e
Amit Kumar Ghosh <135455448+amit-kr-ghosh@users.noreply.github.com>
2026-06-25 15:58:16 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.27%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -5,23 +5,26 @@ public:
             return;
         }
         int l = 0;
-        int r = 1;
+        int r = nums.size()-1;
+        int nums2 [ nums.size()];
 
-        while(r<nums.size()){
-            if(nums[l] == 0 && nums[r]!=0){
-                swap(nums[l],nums[r]);
-                l++;
-                r++;
-
-            }
-            else if(nums[l] == 0 && nums[r] == 0){
-                r++;
+        for(int  i = 0;i<nums.size();i++){
+            if(nums[i]== 0){
+                nums2[r] = 0;
+                r--;
             }
             else{
+                nums2[l] = nums[i];
                 l++;
-                r++;
             }
         }
+
+        for(int i = 0;i<nums.size();i++){
+            nums[i] = nums2[i];
+
+        }
+
+       
         return;
         
     }
